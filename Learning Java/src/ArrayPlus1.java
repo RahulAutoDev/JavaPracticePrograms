e9b33e7f1897fe27b446c19c38360b4d0672eb45
RahulAutoDev <Rahul.26gemini@gmail.com>
2021-07-13 23:39:39 +0530

    New Commit

---
+++ Learning Java/src/ArrayPlus1.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ArrayPlus1 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,4};
+		String joinedString = Arrays.toString(arr).replaceAll("\\[|\\]|,|\\s", "");
+		String res = "";
+		
+		for(int i=0; i<joinedString.length(); i++)
+		{
+			res = res + joinedString.charAt(i);
+		}
+	
+		int num = Integer.parseInt(res);
+		num = num+1;
+		
+		String temp = Integer.toString(num);
+		
+		for(int i=0; i<temp.length(); i++)
+		{
+			System.out.print(temp.charAt(i));
+		}
+		
+
+	}
+
+}
